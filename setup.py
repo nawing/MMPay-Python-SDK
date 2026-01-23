075a9eb1561cecec21d2bb485d840366c2b059f5
nawing <nawingngan@gmail.com>
2026-01-23 17:28:42 +0630

    updating documentation

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="mmpay-python-sdk",
-    version="0.1.0",
+    version="0.1.1",
     description="Python SDK for MMPay (Ported from JS)",
-    author="Your Name",
+    author="MyanMyanPay",
     packages=find_packages(),
     install_requires=[
         "requests",
